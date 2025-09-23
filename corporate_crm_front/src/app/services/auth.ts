0d50051ece6f5ae3d7fef440d0c4cae8d23bd47c
Davide Latina <davide.latina616@gmail.com>
2025-09-23 15:25:07 +0200

    Update auth.ts

--- corporate_crm_front/src/app/services/auth.ts
+++ corporate_crm_front/src/app/services/auth.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
